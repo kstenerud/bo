33bdebbf6c3c243e30f0f613d7dd047bb26508c2
Karl Stenerud <kstenerud@gmail.com>
2018-07-03 10:04:41 -0700

    Command line help, cleanup

--- bo_app/src/main.c
+++ bo_app/src/main.c
@@ -1,15 +1,83 @@
+//  Copyright (c) 2018 Karl Stenerud. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall remain in place
+// in this source code.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+
 #include <bo/bo.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <ctype.h>
 #include <string.h>
+
+
+static const char g_usage[] =
+	"Usage: bo [options] command [command] ...\n"
+	"\n"
+	"Options:\n"
+	"    -i [filename]: Read commands/data from a file (use \"-\" to read from stdin).\n"
+	"    -o [filename]: Write output to a file (use \"-\" to write to stdout).\n"
+	"\n"
+	"Commands:\n"
+	"    i{type}{data width}{endianness}: Specify how to interpret input data\n"
+	"    o{type}{data width}{endianness}{print width}: Specify how to re-interpret data and how to print it\n"
+	"    p{string}: Specify a prefix to prepend to each datum output\n"
+	"    s{string}: Specify a suffix to append to each datum object (except for the last object)\n"
+	"    P{type}: Specify a preset for prefix and suffix.\n"
+	"\n"
+	"Types:\n"
+	"    i: Integer in base 10\n"
+	"    h: Integer in base 16\n"
+	"    o: Integer in base 8\n"
+	"    b: Integer in base 2\n"
+	"    f: IEEE 754 binary floating point\n"
+	"    d: IEEE 754 binary decimal\n"
+	"    B: Data is interpreted or output using its binary representation rather than text.\n"
+	"\n"
+	"Data Widths:\n"
+	"    1 bytes (8-bit)\n"
+	"    2 bytes (16-bit)\n"
+	"    4 bytes (32-bit)\n"
+	"    8 bytes (64-bit)\n"
+	"    16 bytes (128-bit)\n"
+	"\n"
+	"Endianness:\n"
+	"    l: Little Endian\n"
+	"    b: Big Endian\n"
+	"\n"
+	"Print Width:\n"
+	"    Any integer representing the minimum number of digits to print.\n"
+	"    For floating point, the number of digits after the decimal point.\n"
+	"\n"
+	"Presets:\n"
+	"    c: C-style preset: \", \" suffix, 0x prefix for hexadecimal, 0 prefix for octal.\n"
+	"    s: Space preset: \" \" suffix.\n"
+	"\n"
+	"Example: Convert the string \"Testing\" to its hex representation using \"space\" preset:\n"
+	"\n"
+	"    bo \"oh1b2 Ps ih1b \\\"Testing\\\"\"\n"
+	;
+
 
 static void print_usage()
 {
-	printf("\n");
-	printf("Version: %s\n", bo_version());
-	printf("Usage: bo <input>\n");
+	printf("\n%s", g_usage);
 }
 
 static void on_error(void* user_data, const char* message)
@@ -93,7 +161,7 @@ int main(int argc, char* argv[])
 	int in_file_count = 0;
 	FILE* out_stream = stdout;
 	int opt;
-    while((opt = getopt (argc, argv, "i:o:")) != -1)
+    while((opt = getopt (argc, argv, "i:o:h")) != -1)
     {
     	switch(opt)
         {
@@ -101,7 +169,7 @@ int main(int argc, char* argv[])
 		    	if(in_file_count >= max_file_count)
 		    	{
 					fprintf(stderr, "Too many input files. Maximum allowed is %d.\n", max_file_count);
-					return 1;
+					goto failed;
 		    	}
 		    	in_filenames[in_file_count++] = strdup(optarg);
         		break;
@@ -109,9 +177,10 @@ int main(int argc, char* argv[])
 		    	close_stream(out_stream); // Just in case the user does something stupid
 		    	out_stream = new_output_stream(optarg);
         		break;
+        	case 'h':
       		default:
 				print_usage();
-				return 1;
+				goto failed;
       	}
 	}
 	bool has_args = optind < argc;
@@ -155,7 +224,7 @@ int main(int argc, char* argv[])
 	return 0;
 
 failed:
-    printf("failed\n");fflush(stdout);
+	printf("Use bo -h for help.\n");
 	teardown(context, out_stream, in_filenames, in_file_count);
 	return 1;
 }
