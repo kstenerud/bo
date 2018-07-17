5600e6c2dcb83e98d39f337eb3deef9249051805
Karl Stenerud <kstenerud@gmail.com>
2018-07-17 08:57:04 -0700

    Proper c-style parsing of strings

--- libbo/test/src/string.cpp
+++ libbo/test/src/string.cpp
@@ -3,5 +3,5 @@
 TEST(BO_String, string)
 {
     assert_conversion("os ih1 \"Testing\" 01 02 \"ß\" 5", "Testing\\x01\\x02ß\\x05");
-    assert_conversion("os is \"\\41\\x42\\u263a\"", "AB☺");
+    assert_conversion("os is \"\\101\\x42\\u263a\"", "AB☺");
 }
